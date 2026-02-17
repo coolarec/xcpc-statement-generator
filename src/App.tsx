9e42f43ab490e460070607602109564d4842f75e
chumeng <2595248810@qq.com>
2026-02-18 04:38:51 +0800

    feat: add footer component with app info and GitHub link
    Add footer to main layout showing:
    - App name and version from package.json
    - GitHub link with FontAwesome icon
    - Git commit info
    - Developer credit
    
    Also wrap ContestEditor in main element for better semantic HTML.

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,7 @@
 import type { FC } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { name as appName, version as appVersion } from "../package.json";
 import TypstInitStatusProvider from "./components/TypstInitStatusProvider";
 import { ToastProvider } from "./components/ToastProvider";
 import ContestEditor from "./contestEditor";
@@ -10,7 +13,24 @@ const App: FC = () => {
     <ToastProvider>
       <TypstInitStatusProvider>
         <div className="w-screen h-screen overflow-hidden text-gray-800 flex flex-col">
-          <ContestEditor />
+          <main className="flex-1 min-h-0">
+            <ContestEditor />
+          </main>
+          <footer>
+            <div>
+              <a
+                href="https://github.com/lihaoze123/xcpc-statement-generator"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <FontAwesomeIcon icon={faGithub} className="mr-1" />
+                {appName}
+              </a>
+              <span className="ml-2">v{appVersion}</span>
+              <span className="ml-1">({GIT_COMMIT_INFO})</span>
+            </div>
+            <div>Developed by chumeng with ❤️</div>
+          </footer>
         </div>
       </TypstInitStatusProvider>
     </ToastProvider>
